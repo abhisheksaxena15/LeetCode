103425cea7eb68724ea19beb57bf0965a4e1cae3
abhisheksaxena15 <saxenaabhishek9009@gmail.com>
2025-01-03 22:15:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (66.37%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,18 +1,34 @@
+// class Solution {
+// public:
+//     int integerBreak(int n) {
+//         if (n == 2) return 1;
+//         if (n == 3) return 2;
+
+        
+//         int quotient = n / 3;
+//         int remainder = n % 3;
+
+//         if(remainder == 0) return pow(3 , quotient);
+//         if(remainder == 1) return pow(3 , quotient - 1) * 4;
+//         else{
+//             return pow(3 , quotient) * 2;
+//         }
+
+//     }
+// };
+
 class Solution {
 public:
     int integerBreak(int n) {
         if (n == 2) return 1;
         if (n == 3) return 2;
 
-        
-        int quotient = n / 3;
-        int remainder = n % 3;
-
-        if(remainder == 0) return pow(3 , quotient);
-        if(remainder == 1) return pow(3 , quotient - 1) * 4;
-        else{
-            return pow(3 , quotient) * 2;
+        int result = 1;
+        while(n > 4){
+            result = result * 3;
+            n = n - 3;
         }
+        return result = result * n;
 
     }
 };
