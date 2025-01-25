ff00c5dfa83f494123b5bcb41a6576efa17f2051
abhisheksaxena15 <saxenaabhishek9009@gmail.com>
2025-01-25 23:45:39 +0530

    Time: 3 ms (69.54%) | Memory: 65.2 MB (41.04%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -5,10 +5,12 @@ public:
         int slow = nums[0];
         int fast = nums[0];
 
-        do{
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        while(slow != fast){
             slow = nums[slow];
             fast = nums[nums[fast]];
-        } while(slow != fast);
+        }
 
 
         slow = nums[0];
