ca9e44f700d564044cd67eed705fb8c5caae678d
abhisheksaxena15 <saxenaabhishek9009@gmail.com>
2025-05-06 22:51:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (86.09%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int sumofSquare(int n){
+        int sum = 0;
+        while(n!=0){
+            int a = n%10;
+            sum += a*a;
+            n /= 10;            
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        while(n>=1){
+            n = sumofSquare(n);
+            if(n==1 || n==7)
+                return true;
+            else if(n<10) 
+                return false;
+        }
+
+        return false;
+    }
+};
